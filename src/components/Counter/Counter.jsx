213ceeec33f0c9b00699e4da7da91e89581548b7
aidanliddiard <aidanliddiard@gmail.com>
2022-05-11 15:19:52 -0700

    counter working but still have color state

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -7,43 +7,68 @@ const colors = {
   red: 'rgb(239, 68, 68)',
 };
 
+import { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT_COUNT':
+      return {
+        count: state.count + 1,
+      };
+    case 'DECREMENT_COUNT':
+      return {
+        count: state.count - 1,
+      };
+    case 'RESET_COUNT':
+      return {
+        count: (state.count = 0),
+      };
+    default:
+      throw new Error(`Action type ${action.type} is not supported`);
+  }
+};
+
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
   const [currentColor, setCurrentColor] = useState(colors.yellow);
 
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(() => {
-    if (count === 0) {
+    if (state.count === 0) {
       setCurrentColor(colors.yellow);
     }
 
-    if (count > 0) {
+    if (state.count > 0) {
       setCurrentColor(colors.green);
     }
 
-    if (count < 0) {
+    if (state.count < 0) {
       setCurrentColor(colors.red);
     }
-  }, [count]);
+  }, [state.count]);
 
-  const increment = () => {
-    setCount((prevState) => prevState + 1);
-  };
+  //const increment = () => {
+  //  setCount((prevState) => prevState + 1);
+  //};
 
-  const decrement = () => {
-    setCount((prevState) => prevState - 1);
-  };
+  //const decrement = () => {
+  //  setCount((prevState) => prevState - 1);
+  //};
 
-  const reset = () => {
-    setCount(0);
-  };
+  //const reset = () => {
+  //  setCount(0);
+  //};
 
   return (
     <main className={styles.main}>
-      <h1 style={{ color: currentColor }}>{count}</h1>
+      <h1 style={{ color: currentColor }}>{state.count}</h1>
       <div>
         <button
           type="button"
-          onClick={increment}
+          onClick={() => dispatch({ type: 'INCREMENT_COUNT' })}
           aria-label="increment"
           style={{ backgroundColor: colors.green }}
         >
@@ -51,7 +76,7 @@ export default function Counter() {
         </button>
         <button
           type="button"
-          onClick={decrement}
+          onClick={() => dispatch({ type: 'DECREMENT_COUNT' })}
           aria-label="decrement"
           style={{ backgroundColor: colors.red }}
         >
@@ -60,7 +85,11 @@ export default function Counter() {
         <button
           type="button"
           aria-label="reset"
-          onClick={reset}
+          onClick={() =>
+            dispatch({
+              type: 'RESET_COUNT',
+            })
+          }
           style={{ backgroundColor: colors.yellow }}
         >
           Reset
